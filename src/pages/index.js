05e1c55a71fc8a0efc63f46f0c00d03ce53bb1fa
luangelsdorf <luanferreira2136@gmail.com>
2023-10-24 13:58:23 -0300

    Added page title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
+import Head from 'next/head';
 import Image from 'next/image';
 import home from 'public/Home.jpg';
 
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Home | TecnoKit</title>
+      </Head>
       <Image style={{ verticalAlign: 'middle', maxWidth: '100%', height: 'auto' }} src={home} alt="" />
     </div>
   )
