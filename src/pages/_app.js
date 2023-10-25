a58b35b68b525147c10bec07ad7859624f262825
luangelsdorf <luanferreira2136@gmail.com>
2023-10-25 09:23:10 -0300

    Updated Sass & styles

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,28 @@
-import '@/styles/globals.css'
+/* import '@/styles/globals.css'; */
+import '@/styles/styles.scss';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Open_Sans } from 'next/font/google';
+
+const openSans = Open_Sans({
+  subsets: ['latin'],
+  variable: '--open-sans',
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (
+    <>
+      <style jsx global>
+        {`
+          html {
+            font-family: ${openSans.style.fontFamily};
+          }
+          svg text {
+            font-family: ${openSans.style.fontFamily};
+          }
+        `}
+      </style>
+      <Component {...pageProps} />
+    </>
+  )
 }
